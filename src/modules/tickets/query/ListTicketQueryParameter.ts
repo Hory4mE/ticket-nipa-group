6e6816585944aa012afb1ac32f770e1c680c608a
dolnadolnadol <saiparnchollada@gmail.com>
2024-05-15 17:38:19 +0700

    fix: sortby query params

--- src/modules/tickets/query/ListTicketQueryParameter.ts
+++ src/modules/tickets/query/ListTicketQueryParameter.ts
@@ -1,25 +1,26 @@
 /* eslint-disable prettier/prettier */
 import {
-    createSortingConditionValidator,
-    Expose,
-    IsEnum,
-    IsOptional,
-    ISortingConditionType
+  createSortingConditionValidator,
+  Expose,
+  IsEnum,
+  IsOptional,
+  ISortingConditionType,
+  Transform,
 } from "@nipacloud/framework/core/util/validator";
 
 import { ITicket } from "@app/data/abstraction/entities/ITickets";
 import { TicketStatus } from "../models/Definitions";
 
-const { SortingConditionTransformer } = createSortingConditionValidator("created_at");
+const { SortingConditionTransformer } =
+  createSortingConditionValidator("created_at");
 
 export class IListTicketQueryParameter {
-    @IsEnum(TicketStatus)
-    @IsOptional()
-    public readonly status?: TicketStatus;
+  @IsEnum(TicketStatus)
+  @IsOptional()
+  public readonly status?: TicketStatus;
 
-    @IsOptional()
-    // @IsArray()
-    @Expose({ name: "sort_by" })
-    // @Transform(SortingConditionTransformer, { toClassOnly: true })
-    public sortBy?: ISortingConditionType<ITicket>[];
+  @IsOptional()
+  @Expose({ name: "sort_by" })
+  @Transform(({ value }) => JSON.parse(value))
+  public sortBy?: ISortingConditionType<ITicket>[];
 }
--- src/modules/tickets/query/TicketQueryOption.ts
+++ src/modules/tickets/query/TicketQueryOption.ts
@@ -1,34 +1,32 @@
 /* eslint-disable prettier/prettier */
 import { ITicket } from "@app/data/abstraction/entities/ITickets";
 import { TicketStatus } from "@app/modules/tickets/models/Definitions";
-import { PaginationQuery, PaginationQueryMaker } from "../../../query/PaginationQuery";
+import {
+  PaginationQuery,
+  PaginationQueryMaker,
+} from "../../../query/PaginationQuery";
 import { IListTicketQueryParameter } from "./ListTicketQueryParameter";
 
-export interface TicketQueryOption{
-    status?: TicketStatus[];
-    paginationQuery?: PaginationQuery<ITicket>;
-    sort_by?: IListTicketQueryParameter;
+export interface TicketQueryOption {
+  status?: TicketStatus[];
+  paginationQuery?: PaginationQuery<ITicket>;
+  sort_by?: IListTicketQueryParameter;
 }
 
 export class TicketQueryOptionMaker {
-    static fromRoomListQueryParams(params: IListTicketQueryParameter) {
-        const option: TicketQueryOption = {};
-        option.paginationQuery = PaginationQueryMaker.make(params);
-        if (params.status) {
-            option.status = [params.status];
-        } else {
-            option.status = [];
-        }
-        console.log('sortby option',params)
-        if (params?.sortBy){
-            option.sort_by = {sortBy:params.sortBy};
-            console.log('sortby option',params.sortBy)
-            console.log('option',option.sort_by.sortBy)
-        }else{
-
-            option.sort_by.sortBy = []
-        }
-
-        return option;
+  static fromRoomListQueryParams(params: IListTicketQueryParameter) {
+    const option: TicketQueryOption = {};
+    option.paginationQuery = PaginationQueryMaker.make(params);
+    if (params.status) {
+      option.status = [params.status];
+    } else {
+      option.status = [];
     }
+    if (params?.sortBy) {
+      option.sort_by = { sortBy: params.sortBy };
+    } else {
+      option.sort_by = { sortBy: [] };
+    }
+    return option;
+  }
 }
