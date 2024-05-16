5bdd054159c3b48ec1b69f81ca74a81a005a6825
dolnadolnadol <saiparnchollada@gmail.com>
2024-05-16 16:41:01 +0700

    feat: add check token getall

--- src/modules/tickets/TicketServices.ts
+++ src/modules/tickets/TicketServices.ts
@@ -1,19 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { IAppUnitOfWork } from "@app/data/abstraction/IAppUnitOfWork";
-import {
-    AppUnitOfWorkFactoryIdentifier,
-    IAppUnitOfWorkFactory,
-} from "@app/data/abstraction/IAppUnitOfWorkFactory";
+import { AppUnitOfWorkFactoryIdentifier, IAppUnitOfWorkFactory } from "@app/data/abstraction/IAppUnitOfWorkFactory";
 import { ITicket } from "@app/data/abstraction/entities/ITickets";
 import { TicketQueryOptionMaker } from "@app/modules/tickets/query/TicketQueryOption";
 import { using } from "@nipacloud/framework/core/disposable";
-import { NotFoundError } from "@nipacloud/framework/core/http";
+import { NotFoundError, UnauthorizedError } from "@nipacloud/framework/core/http";
 import { Inject, Service } from "@nipacloud/framework/core/ioc";
+import jwt from "jsonwebtoken";
 import { TicketDomainService } from "./TicketDomainService";
 import { CreateTicketRequest, UpdateTicketRequest } from "./dto/TicketRequest";
 import { TicketStatus } from "./models/Definitions";
 import { IListTicketQueryParameter } from "./query/ListTicketQueryParameter";
-
+import { ITicketHeader } from "./query/TicketHeader";
 @Service()
 export class TicketService {
     @Inject(AppUnitOfWorkFactoryIdentifier)
@@ -22,7 +20,13 @@ export class TicketService {
     @Inject()
     private ticketDomainService: TicketDomainService;
 
-    public async list(params: IListTicketQueryParameter): Promise<ITicket[]> {
+    public async list(params: IListTicketQueryParameter, header: ITicketHeader): Promise<ITicket[]> {
+        const token: any = jwt.verify(header.token, process.env.JWT_ACCESS_SECRET);
+        const allowRoles = ["ADMIN", "REVIEWER"];
+        const hasAccess = allowRoles.includes(token.roles);
+        if (!hasAccess) {
+            throw new UnauthorizedError("Invalid Token.");
+        }
         return using(this.unitOfWorkFactory.create())(async (uow: IAppUnitOfWork) => {
             const option = TicketQueryOptionMaker.fromTicketListQueryParams(params);
             const tickets = await this.ticketDomainService.list(uow, option);
@@ -31,12 +35,24 @@ export class TicketService {
             } else {
                 return tickets;
             }
-
         });
     }
-    public async getById(ticketId: string): Promise<ITicket> {
+    public async getById(ticketId: string, header: ITicketHeader): Promise<ITicket> {
+        const token: any = jwt.verify(header.token, process.env.JWT_ACCESS_SECRET);
+        const allowRoles = ["ADMIN", "REVIEWER"];
+        const allowRolesUser = ["USER"];
+        const hasAccessAll = allowRoles.includes(token.roles);
+        const hasAccessSelf = allowRolesUser.includes(token.roles);
+        if (!hasAccessAll && !hasAccessSelf) {
+            throw new UnauthorizedError("Invalid Token.");
+        }
         return using(this.unitOfWorkFactory.create())(async (uow: IAppUnitOfWork) => {
             const tickets = await this.ticketDomainService.findById(uow, ticketId);
+            if (hasAccessSelf) {
+                if (tickets.user_id != token.user_id) {
+                    throw new UnauthorizedError("Invalid Token.");
+                }
+            }
             return tickets;
         });
     }
@@ -47,34 +63,22 @@ export class TicketService {
         });
     }
 
-    public async update(
-        ticketId: string,
-        body: UpdateTicketRequest
-    ): Promise<void> {
-        return using(this.unitOfWorkFactory.create())(
-            async (uow: IAppUnitOfWork) => {
-                const entity = body.toTicketEntity();
-                return this.ticketDomainService.update(uow, ticketId, entity);
-            }
-        );
+    public async update(ticketId: string, body: UpdateTicketRequest): Promise<void> {
+        return using(this.unitOfWorkFactory.create())(async (uow: IAppUnitOfWork) => {
+            const entity = body.toTicketEntity();
+            return this.ticketDomainService.update(uow, ticketId, entity);
+        });
     }
 
-    public async updateStatus(
-        ticketId: string,
-        status: TicketStatus
-    ): Promise<void> {
-        return using(this.unitOfWorkFactory.create())(
-            async (uow: IAppUnitOfWork) => {
-                return this.ticketDomainService.update(uow, ticketId, { status });
-            }
-        );
+    public async updateStatus(ticketId: string, status: TicketStatus): Promise<void> {
+        return using(this.unitOfWorkFactory.create())(async (uow: IAppUnitOfWork) => {
+            return this.ticketDomainService.update(uow, ticketId, { status });
+        });
     }
 
     public async delete(ticketId: string) {
-        return using(this.unitOfWorkFactory.create())(
-            async (uow: IAppUnitOfWork) => {
-                return this.ticketDomainService.delete(uow, ticketId);
-            }
-        );
+        return using(this.unitOfWorkFactory.create())(async (uow: IAppUnitOfWork) => {
+            return this.ticketDomainService.delete(uow, ticketId);
+        });
     }
 }
